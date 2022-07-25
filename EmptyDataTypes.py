17823cbc887b84ff2e3698c243d0ad90edef0fb8
Rahul Soni <rahulsoni07064@gmail.com>
2022-07-25 17:26:05 +0530

    Empty data types

---
+++ EmptyDataTypes.py
@@ -0,0 +1,21 @@
+#Set
+s = set()
+print("Empty Set :" , type(s))
+
+#dictionary
+dict1 = {}
+print("Empty dictionary :" , type(dict1))
+dict2 = dict()
+print("Empty dictionary :" , type(dict2))
+
+# list
+l1 = []
+l2 = list()
+print("Empty List :" , type(l1))
+print("Empty List :" , type(l2))
+
+# tuple
+t1 = tuple()
+t2 = ()
+print("Empty Tuple :" , type(t1))
+print("Empty Tuple :" , type(t2))
